04d3142a3e8db2851b6173f5f14b47f1bb3bd879
cmillani <carloseduardomillani@gmail.com>
2015-06-03 07:50:38 -0300

    Arrumar centro - esta andando sozinho, + FS
    Fail safe nao funciona corretamente.

--- Beetle C/Beetle C.cpp
+++ Beetle C/Beetle C.cpp
@@ -9,35 +9,44 @@
 #include <avr/interrupt.h>
 
 // Macros input, output, low, high
-#define output_low(port,pin) port &= ~(1<<pin)
-#define output_high(port,pin) port |= (1<<pin)
-#define set_input(portdir,pin) portdir &= ~(1<<pin)
-#define set_output(portdir,pin) portdir |= (1<<pin)
+#define output_low(port,pin) {port &= ~(1<<pin);}
+#define output_high(port,pin) {port |= (1<<pin);}
+#define set_input(portdir,pin) {portdir &= ~(1<<pin);}
+#define set_output(portdir,pin) {portdir |= (1<<pin);}
 
-volatile uint8_t time_int_1 = 0;
-volatile uint8_t time_int_1_before = 0;
-volatile uint8_t time_int_0 = 0;
-volatile uint8_t time_int_0_before = 0;
+volatile uint16_t time_int_1 = 0;
+volatile uint16_t time_int_1_before = 0;
+volatile uint16_t time_int_0 = 0;
+volatile uint16_t time_int_0_before = 0;
 
-ISR(INT0_vect){
-	if (PINB & (1 << PINB6))
+volatile uint8_t last_pow_dir = 0;
+volatile uint8_t last_pow_esq = 0;
+
+ISR(INT0_vect){ //Interrupcao chamada quando pino muda de estado
+	if (PINB & (1 << PINB6)) //Pino foi para alto
 	{
-		time_int_0_before = TCNT0L;
+		time_int_0_before = TCNT0L; //Salva tempo
 	}
-	else
+	else //Pino foi para baixo
 	{
-		if ((TCNT0L - time_int_0_before) > 0) time_int_0 = TCNT0L - time_int_0_before;
+		if ((TCNT0L - time_int_0_before) > 120 && (TCNT0L - time_int_0_before) < 255) 
+		{
+			time_int_0 = TCNT0L - time_int_0_before; //Salva diferenca de tempo para calculo do pwm
+		}
 	}
 }
 
-ISR(INT1_vect){
+ISR(INT1_vect){ //Vide funcao acima
 	if (PINA & (1 << PINA2))	
 	{
 		time_int_1_before = TCNT0L;
 	}
 	else
 	{
-		if ((TCNT0L - time_int_1_before) > 0) time_int_1 = TCNT0L - time_int_1_before;
+		if ((TCNT0L - time_int_1_before) > 120 && (TCNT0L - time_int_1_before) < 255)
+		{
+			time_int_1 = TCNT0L - time_int_1_before;
+		}
 	}
 }
 
@@ -72,55 +81,70 @@ void delay_ms(uint8_t ms) {
 //INT0 PB6     INT1 PA2
 int main(){
 	configura_portas();
-	MCUCR |= (1 << ISC00) | (0 << ISC01); //MCUCR = 0b00000011
-	GIMSK |= (1 << INT0) | (1 << INT1); //GISMK = 0b11000000
-	delay_ms(100);
+	MCUCR |= (1 << ISC00) | (0 << ISC01); //Configura modo das interrupcoes
+	GIMSK |= (1 << INT0) | (1 << INT1); //Hobilita interrupcoes nos pinos (mas ainda nao estao ativas)
 
-	TCCR0B |= (1 << CS00) | (1 << CS01); //Config timer
+	TCCR0B |= (1 << CS00) | (1 << CS01); //Configura timer que ira ler o sinal do receptor (pre scaler 64)
 
-	TCCR1A |= (1 << COM1A1) | (0 << COM1A0) | (1 << COM1B1) | (0 << COM1B0) | (1 << PWM1A) | (1 << PWM1B);
-	TCCR1B |= (1 << CS10);
-	TCCR1D |= (1 << WGM10);
-	OCR1C = 255; //set top
-	sei(); //habilita interrupçoes
+	TCCR1A |= (1 << COM1A1) | (0 << COM1A0) | (1 << COM1B1) | (0 << COM1B0) | (1 << PWM1A) | (1 << PWM1B); // Configura PWM modo nao inversor, apenas PWM, sem o barrado
+	TCCR1B |= (1 << CS10); //Configura timer do gerador de PWM
+	TCCR1D |= (1 << WGM10); //Configura como FAST pwm
+	OCR1C = 255; //Seta o TOP da contagem do pwm
+	OCR1A = 0;
+	OCR1A = 0;
+	WDTCR |= (1 << WDE) | (1 << WDP0) | (1 << WDP1) | (1 << WDP2); //Habilita watchdog com reset a cada 0.5 s
 	
-	int16_t pwm1, pwm2;
+	sei(); //Ativa interrupçoes
+	delay_ms(100);
+	
+	int16_t pwm1, pwm2; //PWM1 = canal 1, pino B
+						//PWM2 = canal 3, pino A
 	int16_t direita, esquerda;
+	bool safe_to_start = false;
+	do{
+		if ((((time_int_0 + 10) > 200) && ((time_int_0 - 10) < 200))) //Verifica se o robo esta no zero para poder comecar
+		{
+			if ((((time_int_1 + 10) > 200) && ((time_int_1 - 10) < 200))) safe_to_start = true; //If em duas linhas para organizar. Dois canais tem que estar em zero
+		}
+	}while (!safe_to_start);
+	//time_int_0 = 200; //equivalente a zero
+	//time_int_1 = 200; //equivalente a zero
 	while(1){
-		pwm1 = (((time_int_0 - 125) * 2.048) - 128)*2;
-		pwm2 = (((time_int_1 - 125) * 2.048) - 128)*2;
+		//asm("WDR");
+		pwm1 = 4.85*time_int_0 - 967; //Transforma sinal recebido na range desejada, entrada varia de 125 a 250
+		pwm2 = 4.85*time_int_1 - 967; 
 		direita = pwm2 + pwm1;
 		esquerda = pwm2 - pwm1;
-		if (direita > 20)
+		
+		if (!(((last_pow_dir + 15) > direita) && ((last_pow_dir - 15) < direita))) //Se ficou muito tempo no mesmo sinal nao reseta
 		{
-			OCR1A = (direita >= 255)?255:direita;
+			last_pow_dir = direita;
+			asm("WDR");
+		}
+		if (!(((last_pow_esq + 15) > esquerda) && ((last_pow_esq - 15) < esquerda))) //Se ficou muito tempo no mesmo sinal nao reseta
+		{
+			last_pow_esq = esquerda;
+			asm("WDR");
+		}
+		if (direita > 0) //Gira direita sentido horario
+		{
+			OCR1A = (direita >= 255)?255:(direita & 0xFF);
 			output_low(PORTB, PINB2);
 		}
-		else if (direita < -20)
+		else //Gira direita sentido anti-horario
 		{
-			OCR1A = (direita <= -255)?0:(direita + 255);
+			OCR1A = (direita <= -255)?0:((direita & 0xFF)+ 255);
 			output_high(PORTB, PINB2);
 		}
-		else
+		if (esquerda > 0) //Gira esquerda sentido horario
 		{
-			OCR1A = 0;
-			output_low(PORTB, PINB2);
-		}
-		if (esquerda > 20)
-		{
-			OCR1B = (esquerda >= 255)?255:esquerda;
+			OCR1B = (esquerda >= 255)?255:(esquerda & 0xFF);
 			output_low(PORTB, PINB0);
 		}
-		else if (esquerda < -20)
+		else //Gira direita sentido anti-horario
 		{
-			OCR1B = (esquerda <= -255)?0:(esquerda + 255);
+			OCR1B = (esquerda <= -255)?0:((esquerda & 0xFF) + 255);
 			output_high(PORTB, PINB0);
 		}
-		else
-		{
-			OCR1B = 0;
-			output_high(PORTB, PINB0);
-		}
-		
 	}//end while
 }//end main
